17c472141eee35cf2193c850a036626987f81a7b
shainaboover <64878394+shainaboover@users.noreply.github.com>
2020-09-30 22:27:50 -0500

    northwind revision

--- sprint-challenge/northwind.py
+++ sprint-challenge/northwind.py
@@ -20,6 +20,10 @@ query_1 = """SELECT
 
 result_1 = cursor.execute(query_1).fetchall()
 print("10 most expensive items", result_1)
+# 10 most expensive items [('C⌠te de Blaye',), ('Thⁿringer Rostbratwurst',),
+# ('Mishi Kobe Niku',), ("Sir Rodney's Marmalade",), ('Carnarvon Tigers',), 
+# ('Raclette Courdavault',), ('Manjimup Dried Apples',), ('Tarte au sucre',), 
+# ('Ipoh Coffee',), ('R÷ssle Sauerkraut')]
 
 
 # What is the average age of an employee at the time of their hiring?
@@ -29,6 +33,7 @@ query_2 = """SELECT
 
 result_2 = cursor.execute(query_2).fetchall()
 print('Employee average hire age', result_2)
+# Employee average hire age [(37.22222222222222,)]
 
 
 # PART 2
@@ -41,6 +46,10 @@ query_3 = """SELECT
 
 result_3 = cursor.execute(query_3).fetchall()
 print('10 most expensive and supplier', result_3)
+# 10 most expensive and supplier [('C⌠te de Blaye', 18), ('Thⁿringer Rostbratwurst', 12), 
+# ('Mishi Kobe Niku', 4), ("Sir Rodney's Marmalade", 8), ('Carnarvon Tigers', 7), 
+# ('Raclette Courdavault', 28), ('Manjimup Dried Apples', 24), ('Tarte au sucre', 29), 
+# ('Ipoh Coffee', 20), ('R÷ssle Sauerkraut', 12)]
 
 
 # What is the largest category (by number of unique products in it)?
@@ -53,3 +62,4 @@ query_4 = """SELECT
 
 result_4 = cursor.execute(query_4).fetchall()
 print('Largest category', result_4)
+# Largest category [('Confections', 13)]
